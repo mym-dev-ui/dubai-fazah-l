1c36290a7fcfb34f7422e8c7aca179e6729aaf6a
mym-dev-ui <saddam.saqqar@icloud.com>
2026-07-13 09:32:16 +0300

    Replace Firebase config with hardcoded values
    Updated Firebase configuration with hardcoded values.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,17 @@
+// firebase.ts
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL:       process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId:     process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDbqRHcS5xozE26ANS5RxYGjnQMlwwb8gM",
+  authDomain: "asaad-dobae.firebaseapp.com",
+  databaseURL: "https://asaad-dobae-default-rtdb.firebaseio.com",
+  projectId: "asaad-dobae",
+  storageBucket: "asaad-dobae.firebasestorage.app",
+  messagingSenderId: "879581253100",
+  appId: "1:879581253100:web:7b793aeb3cd11c593e04c9",
+  measurementId: "G-GMX3ZBRL5R"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
